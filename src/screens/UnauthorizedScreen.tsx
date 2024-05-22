b31243e8883dbc720062a0a098e890fc8f409963
Lamin Sanneh <lamin.evra@gmail.com>
2024-05-22 15:03:18 +0000

    add component for showing unauthorized screen if user hs no access

---
+++ src/screens/UnauthorizedScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Text} from 'react-native-paper';
+
+const UnauthorizedScreen = () => {
+  return (
+    <View>
+      <Text variant="headlineLarge">Unauthorized Screen</Text>
+    </View>
+  );
+};
+
+export default UnauthorizedScreen;
